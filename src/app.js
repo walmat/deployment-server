54023e361e7a463efce278e8fc0e98ea020aa167
Matthew <matthew.wallt@gmail.com>
2020-03-27 21:31:14 -0400

    fix crash...

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const http = require('http');
 const AWS = require('aws-sdk');
 const expressUserAgent = require('express-useragent');
-const cors = require('cors');
-const path = require('path');
 
 const PORT = process.env.PORT || 9080;
 const DEPLOY_NAME = process.env.DEPLOY_NAME;
@@ -12,39 +9,37 @@ const DEPLOY_NAME = process.env.DEPLOY_NAME;
 const app = express();
 app.server = http.createServer(app);
 
+app.use(expressUserAgent.express());
+
 const paths = {};
 [{ key: 'mac', ext: 'dmg' }, { key: 'win', ext: 'exe' }].forEach(({ key, ext }) => {
   paths[key] = `${DEPLOY_NAME}.${ext}`;
 });
 
+//setting the credentials
+//The region should be the region of the bucket that you created
+//Visit this if you have any confusion - https://docs.aws.amazon.com/general/latest/gr/rande.html
 AWS.config.update({
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_KEY,
-  region: process.env.BUCKET_REGION,
+  region: 'us-east-1',
 });
 
 //Creating a new instance of S3:
 const s3 = new AWS.S3();
 
-app.use(morgan('dev'));
-app.use(cors());
-app.use(expressUserAgent.express());
-
-// Create the platform detector handler
+//GET method route for downloading/retrieving file
 app.get('/', (req, res) => {
-  // Get the useragent's platform
   const {
     useragent: { platform },
   } = req;
 
-  // Set the right file path ()
-  let filePath = null;
   switch (platform) {
     case 'Microsoft Windows': {
-      return retrieveFile(paths.win, res);
+      retrieveFile(paths.win, res);
     }
     case 'Apple Mac': {
-      return retrieveFile(paths.mac, res);
+      retrieveFile(paths.mac, res);
     }
     default: {
       return res
@@ -56,26 +51,23 @@ app.get('/', (req, res) => {
   }
 });
 
-app.server.listen(PORT, () => {
-  console.log(`Started on port ${app.server.address().port}`);
-
-  app.on('shutdown', () => process.exit(0));
+//listening to server 3000
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
 
+//The retrieveFile function
 function retrieveFile(filename, res) {
   const getParams = {
-    Bucket: process.env.BUCKET_NAME,
+    Bucket: 'omega-autoupdater',
     Key: filename,
   };
 
-  console.log(getParams);
-
   s3.getObject(getParams, function(err, data) {
     if (err) {
-      return res.status(404).send(`Unable to retrieve file! Please contact devs regarding this`);
+      return res.status(400).send({ success: false, err: err });
     } else {
-      console.log(data);
-      return res.send(data.Body.toString('utf-8'));
+      return res.send(data.Body);
     }
   });
 }
